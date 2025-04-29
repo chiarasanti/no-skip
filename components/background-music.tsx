d2840081c06440596ea68ea296009e8291b71d30
Chiara Santi <santichiara@hotmail.com>
2025-04-29 23:09:14 +0200

    even more

--- components/background-music.tsx
+++ components/background-music.tsx
@@ -10,7 +10,7 @@ export function BackgroundMusic() {
     // Initialize audio element
     audioRef.current = new Audio("/music/background.mp3");
     audioRef.current.loop = true;
-    audioRef.current.volume = 0.08; // Set a comfortable volume level
+    audioRef.current.volume = 0.05; // Set a comfortable volume level
 
     // Function to start playing
     const startPlaying = () => {
