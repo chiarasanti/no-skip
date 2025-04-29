432ee77b777007572bf6dcaacef9210d1773612a
Chiara Santi <santichiara@hotmail.com>
2025-04-29 21:00:56 +0200

    fix to add button

--- components/pixel-button.tsx
+++ components/pixel-button.tsx
@@ -14,7 +14,7 @@ export function PixelButton({ children, className, ...props }: PixelButtonProps)
   return (
     <button
       className={cn(
-        "border-2 border-white bg-black px-6 py-3 font-mono text-xl text-white transition-all hover:bg-white hover:text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-black",
+        "border-2 border-white bg-black px-6 py-3 font-mono text-xl text-white transition-all hover:bg-white hover:text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-black disabled:opacity-50",
         className,
       )}
       {...props}
